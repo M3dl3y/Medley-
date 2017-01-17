5a7b25ad01aa75d30c4c66a8c0f81d217a2dccdc
Jesse Davila <jessedavila3@gmail.com>
2017-01-17 10:23:20 -0600

    create should redirect to splash page

--- src/main/java/com/medman/controllers/UserController.java
+++ src/main/java/com/medman/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.medman.controllers;
 
+import com.medman.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 /**
  * Created by jessedavila on 1/17/17.
@@ -14,5 +19,21 @@ public class UserController {
     public String showRegister(Model model) {
         model.addAttribute("user", new User());
         return null; // return to user register page.
+    }
+
+    @PostMapping("/create")
+    public String createUser(
+            @Valid User user,
+            Errors validation,
+            Model model
+    ) {
+        if (validation.hasErrors()) {
+            model.addAttribute("errors", validation);
+            model.addAttribute("user", user);
+            return "user/create";
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        usersDao.save(user);
+        return null; // redirect to splashpage
     }
 }
