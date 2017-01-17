dbc67022e3d35fb7f3f0816892fc4ad4a52a90ad
Jesse Davila <jessedavila3@gmail.com>
2017-01-17 10:26:20 -0600

    Visitor Controller for visitor pages including splash page

---
+++ src/main/java/com/medman/controllers/VisitorController.java
@@ -0,0 +1,14 @@
+package com.medman.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jessedavila on 1/17/17.
+ */
+@Controller
+@RequestMapping("/")
+public class VisitorController {
+
+}
