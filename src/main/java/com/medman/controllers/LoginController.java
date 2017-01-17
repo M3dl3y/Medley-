b8ade9b8d4391aa2ceda7532ef01c5ff2758a736
Nicholas Balderas <geist732@gmail.com>
2017-01-17 13:57:34 -0600

    Finished basic skeleton of splash page, login, register, and about by adding buttons, text, navigation bar, and footer.

--- src/main/java/com/medman/controllers/LoginController.java
+++ src/main/java/com/medman/controllers/LoginController.java
@@ -1,4 +1,16 @@
 package com.medman.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+
+
+    @GetMapping("/login")
+        public String showLoginPage(Model model) {
+        return "login";
+    }
+
 }
