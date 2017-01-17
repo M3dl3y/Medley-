f1a386272bc64ad96ba2f16d4a18d799674db315
Jesse Davila <jessedavila3@gmail.com>
2017-01-17 10:49:28 -0600

    created BaseController for authentication purposes extends into UserController

--- src/main/java/com/medman/controllers/UserController.java
+++ src/main/java/com/medman/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
@@ -13,7 +14,11 @@ import javax.validation.Valid;
  * Created by jessedavila on 1/17/17.
  */
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
+    @GetMapping("/dashboard")
+    public String showDash() {
 
+    }
 }
