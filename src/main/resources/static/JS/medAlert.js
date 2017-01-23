f2d3da5c814b633836da969166074e023cbed3a1
Jesse Davila <jessedavila3@gmail.com>
2017-01-23 13:43:58 -0600

    variable changes

--- src/main/resources/static/JS/medAlert.js
+++ src/main/resources/static/JS/medAlert.js
@@ -4,16 +4,20 @@
  */
 
 
+
+// if prescription supply is under 3, add to alert counter
 var prescriptionsDaySupply = $(".prescriptionDaySupply");
 var daySupply;
+var alertNum = 3;
 
 var counter = 0;
 prescriptionsDaySupply.each(function( index ) {
     daySupply = $(this).text();
-    if (daySupply <= 3) {
+    if (daySupply <= alertNum) {
         counter++;
     }
 });
 
 document.getElementById("medicationAlertNumber").innerHTML = counter;
+
 
