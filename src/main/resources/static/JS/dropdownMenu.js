b00fe62f049e09ed084fabaf0361cbd8f72fe21c
Nicholas Balderas <geist732@gmail.com>
2017-01-24 13:30:03 -0600

    Fixed styling/bugs. Implementing materialize.

--- src/main/resources/static/JS/dropdownMenu.js
+++ src/main/resources/static/JS/dropdownMenu.js
@@ -3,4 +3,16 @@
  */
 $(".dropdown-button").dropdown();
 
-$(".button-collapse").sideNav();
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
+});
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+});
---
+++ src/main/resources/static/JS/formSubmission.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Nick on 1/24/17.
+ */
+$('#addMedicationButton').click(function(){
+    document.addPrescriptionForm.submit();
+    return false;
+});
+
+$('#addAppointment').click(function(){
+    document.addAppointment.submit();
+    return false;
+});
