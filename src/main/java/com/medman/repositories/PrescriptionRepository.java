aeaa316b081472aeaaf5b79acf25038d83adae04
Jesse Davila <jessedavila3@gmail.com>
2017-01-27 09:50:01 -0600

    Meds populate into alert panel

--- src/main/java/com/medman/repositories/PrescriptionRepository.java
+++ src/main/java/com/medman/repositories/PrescriptionRepository.java
@@ -10,12 +10,13 @@ import java.util.List;
 
 @Repository
 public interface PrescriptionRepository extends CrudRepository<Prescription, Long> {
+
     @Query("select p from Prescription p, User u where u.id=?1 and u.id = p.user.id")
     public List<Prescription> findByPatient(Long id);
 
-
-    @Query("select p from Prescription p, User u where p.daySupply <= 3 and p.user.id =?1")
+    @Query("select p from Prescription p, User u where p.daySupply <= 3 and u.Id =?1")
     public List<Prescription> findByDaySupplyAlert(Long id);
+
 //    @Query("select p from Prescription p, User u where p.daySupply <= 3 and p.user.id =?1")
 //    public List<Prescription> findByUser(Long id);
 }
